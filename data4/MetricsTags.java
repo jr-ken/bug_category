e3b7f72081541c3b9ccd20f200067d9ecb07b03e
大野堅太郎 <54259281+jr-ken@users.noreply.github.com>
2021-04-21 13:21:39 +0900

    Add files via upload

---
+++ data4/MetricsTags.java
@@ -0,0 +1,418 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
+package org.eclipse.hono.service.metric;
+
+import java.util.Objects;
+
+import org.eclipse.hono.client.ClientErrorException;
+import org.eclipse.hono.util.CommandConstants;
+import org.eclipse.hono.util.EventConstants;
+import org.eclipse.hono.util.Hostnames;
+import org.eclipse.hono.util.TelemetryConstants;
+
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.Tags;
+
+/**
+ * Common definition of metrics tags.
+ */
+public final class MetricsTags {
+
+    /**
+     * The type of endpoint that a message is published to.
+     */
+    public enum EndpointType {
+
+        /**
+         * The endpoint for telemetry messages.
+         */
+        TELEMETRY(TelemetryConstants.TELEMETRY_ENDPOINT),
+        /**
+         * The endpoint for events.
+         */
+        EVENT(EventConstants.EVENT_ENDPOINT),
+        /**
+         * The endpoint for command &amp; control messages.
+         */
+        CONTROL(CommandConstants.COMMAND_ENDPOINT),
+        /**
+         * The unknown endpoint.
+         */
+        UNKNOWN("unknown");
+
+        static final String TAG_NAME = "type";
+
+        private final String canonicalName;
+        private final Tag tag;
+
+        EndpointType(final String canonicalName) {
+            this.canonicalName = canonicalName;
+            this.tag = Tag.of(TAG_NAME, canonicalName);
+        }
+
+        /**
+         * Gets a <em>Micrometer</em> tag for the component type.
+         * 
+         * @return The tag.
+         */
+        public Tag asTag() {
+            return tag;
+        }
+
+        /**
+         * Gets this type's canonical name.
+         * 
+         * @return The name.
+         */
+        public String getCanonicalName() {
+            return canonicalName;
+        }
+
+        /**
+         * Gets the endpoint type from a string value.
+         * 
+         * @param name The name of the endpoint type.
+         * 
+         * @return The enum literal of the endpoint type. Returns {@link #UNKNOWN} if it cannot find the endpoint type.
+         *         Never returns {@code null}.
+         */
+        public static EndpointType fromString(final String name) {
+            switch (name) {
+            case TelemetryConstants.TELEMETRY_ENDPOINT:
+            case TelemetryConstants.TELEMETRY_ENDPOINT_SHORT:
+                return TELEMETRY;
+            case EventConstants.EVENT_ENDPOINT:
+            case EventConstants.EVENT_ENDPOINT_SHORT:
+                return EVENT;
+            case CommandConstants.COMMAND_ENDPOINT:
+            case CommandConstants.COMMAND_ENDPOINT_SHORT:
+                return CONTROL;
+            default:
+                return UNKNOWN;
+            }
+        }
+    }
+
+    /**
+     * The type of component.
+     */
+    public enum ComponentType {
+
+        /**
+         * A service component.
+         */
+        SERVICE,
+        /**
+         * A protocol adapter.
+         */
+        ADAPTER();
+
+        static final String TAG_NAME = "component-type";
+
+        private final Tag tag;
+
+        ComponentType() {
+            this.tag = Tag.of(TAG_NAME, name().toLowerCase());
+        }
+
+        /**
+         * Gets a <em>Micrometer</em> tag for the component type.
+         * 
+         * @return The tag.
+         */
+        public Tag asTag() {
+            return tag;
+        }
+    }
+
+    /**
+     * A status indicating the outcome of processing a message received from a device.
+     *
+     */
+    public enum ProcessingOutcome {
+
+        /**
+         * The outcome indicating that a message has been forwarded to the receiver.
+         */
+        FORWARDED("forwarded"),
+        /**
+         * The outcome indicating that a message could not be delivered to the receiver.
+         */
+        UNDELIVERABLE("undeliverable"),
+        /**
+         * The outcome indicating that a message could not be processed, e.g. because it is malformed.
+         */
+        UNPROCESSABLE("unprocessable");
+
+        static final String TAG_NAME = "status";
+
+        private final Tag tag;
+
+        ProcessingOutcome(final String tagValue) {
+            this.tag = Tag.of(TAG_NAME, tagValue);
+        }
+
+        /**
+         * Gets an outcome for an error.
+         * 
+         * @param t The error.
+         * @return The outcome.
+         */
+        public static ProcessingOutcome from(final Throwable t) {
+            if (t instanceof ClientErrorException) {
+                return UNPROCESSABLE;
+            } else {
+                return UNDELIVERABLE;
+            }
+        }
+
+        /**
+         * Gets a <em>Micrometer</em> tag for the outcome.
+         * 
+         * @return The tag.
+         */
+        public Tag asTag() {
+            return tag;
+        }
+    }
+
+    /**
+     * Status indicating the outcome of processing a TTD value contained in a message received from a device.
+     *
+     */
+    public enum TtdStatus {
+
+        /**
+         * Status indicating that the message from the device did not contain a TTD value.
+         */
+        NONE(),
+        /**
+         * Status indicating that the TTD expired without any pending commands for the device.
+         */
+        EXPIRED("expired"),
+        /**
+         * Status indicating a pending command for the device before the TTD expired.
+         */
+        COMMAND("command");
+
+        static final String TAG_NAME = "ttd";
+
+        private final Tag tag;
+
+        TtdStatus() {
+            this.tag = null;
+        }
+
+        TtdStatus(final String tagValue) {
+            this.tag = Tag.of(TAG_NAME, tagValue);
+        }
+
+        /**
+         * Gets a <em>Micrometer</em> tag for the TTD status.
+         * 
+         * @return The tag or {@code null} if the status is {@link #NONE}.
+         */
+        public Tag asTag() {
+            return tag;
+        }
+
+        /**
+         * Adds a tag for the TTD status to a given set of tags.
+         * <p>
+         * The tag is only added if the status is not {@link #NONE}.
+         * 
+         * @param tags The tags to add to.
+         * @return The tags.
+         * @throws NullPointerException if tags is {@code null}.
+         */
+        public Tags add(final Tags tags) {
+            Objects.requireNonNull(tags);
+            if (tag == null) {
+                return tags;
+            } else {
+                return tags.and(tag);
+            }
+        }
+    }
+
+    /**
+     * Quality of service used for sending a message.
+     */
+    public enum QoS {
+
+        /**
+         * QoS indicating unknown delivery semantics.
+         */
+        UNKNOWN(),
+        /**
+         * QoS (level 0) indicating at-most-once delivery semantics.
+         */
+        AT_MOST_ONCE("0"),
+        /**
+         * QoS (level 1) indicating at-least-once delivery semantics.
+         */
+        AT_LEAST_ONCE("1");
+
+        static final String TAG_NAME = "qos";
+
+        private Tag tag;
+
+        QoS() {
+            this.tag = null;
+        }
+
+        QoS(final String tagValue) {
+            this.tag = Tag.of(TAG_NAME, tagValue);
+        }
+
+        /**
+         * Gets the QoS for a level.
+         * 
+         * @param level The level.
+         * @return The corresponding quality of service.
+         */
+        public static QoS from(final int level) {
+            switch (level) {
+            case 0:
+                return AT_MOST_ONCE;
+            case 1:
+                return AT_LEAST_ONCE;
+            default:
+                return UNKNOWN;
+            }
+        }
+
+        /**
+         * Gets a <em>Micrometer</em> tag for the QoS level.
+         * 
+         * @return The tag or {@code null} if the status is {@link #UNKNOWN}.
+         */
+        public Tag asTag() {
+            return tag;
+        }
+
+        /**
+         * Adds a tag for the QoS level to a given set of tags.
+         * <p>
+         * The tag is only added if the status is not {@link #UNKNOWN}.
+         * 
+         * @param tags The tags to add to.
+         * @return The tags.
+         * @throws NullPointerException if tags is {@code null}.
+         */
+        public Tags add(final Tags tags) {
+            Objects.requireNonNull(tags);
+            if (tag == null) {
+                return tags;
+            } else {
+                return tags.and(tag);
+            }
+        }
+    }
+
+    /**
+     * The direction of a message.
+     *
+     */
+    public enum Direction {
+
+        /**
+         * A one-way message.
+         */
+        ONE_WAY("one-way"),
+        /**
+         * A request message.
+         */
+        REQUEST("request"),
+        /**
+         * A response message.
+         */
+        RESPONSE("response");
+
+        static final String TAG_NAME = "direction";
+
+        private final Tag tag;
+
+        Direction(final String tagValue) {
+            this.tag = Tag.of(TAG_NAME, tagValue);
+        }
+
+        /**
+         * Gets a <em>Micrometer</em> tag for the direction.
+         * 
+         * @return The tag.
+         */
+        public Tag asTag() {
+            return tag;
+        }
+    }
+
+    /**
+     * The name of the tag that holds the name of the component that reports a metric.
+     */
+    static final String TAG_COMPONENT_NAME = "component-name";
+    /**
+     * The tag that holds the name of the host that the component reporting a metric is running on.
+     */
+    static final String TAG_HOST           = "host";
+    /**
+     * The name of the tag that holds the identifier of the tenant that a metric has been reported for.
+     */
+    static final String TAG_TENANT         = "tenant";
+    /**
+     * The name of the tag that holds the type of message that a metric has been reported for.
+     */
+    static final String TAG_TYPE           = "type";
+
+    private MetricsTags() {
+    }
+
+    /**
+     * Creates the default tag set for a protocol adapter.
+     * 
+     * @param name The name of the protocol adapter.
+     * @return A ready to use tag set.
+     */
+    public static Tags forProtocolAdapter(final String name) {
+        return Tags.of(
+                Tag.of(MetricsTags.TAG_HOST, Hostnames.getHostname()),
+                MetricsTags.ComponentType.ADAPTER.asTag(),
+                Tag.of(MetricsTags.TAG_COMPONENT_NAME, name));
+    }
+
+    /**
+     * Creates the default tag set for a service.
+     * 
+     * @param name The name of the service.
+     * @return A ready to use tag set.
+     */
+    public static Tags forService(final String name) {
+        return Tags.of(
+                Tag.of(MetricsTags.TAG_HOST, Hostnames.getHostname()),
+                MetricsTags.ComponentType.SERVICE.asTag(),
+                Tag.of(MetricsTags.TAG_COMPONENT_NAME, name));
+    }
+
+    /**
+     * Creates a tag for a tenant identifier.
+     * 
+     * @param tenant The tenant.
+     * @return The tag.
+     * @throws NullPointerException if outcome is {@code null}.
+     */
+    public static Tag getTenantTag(final String tenant) {
+        Objects.requireNonNull(tenant);
+        return Tag.of(MetricsTags.TAG_TENANT, tenant);
+    }
+}
---
+++ data4/MicrometerBasedMetrics.java
@@ -0,0 +1,309 @@
+/*******************************************************************************
+ * Copyright (c) 2018, 2019 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
+package org.eclipse.hono.service.metric;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Supplier;
+
+import org.eclipse.hono.service.metric.MetricsTags.Direction;
+import org.eclipse.hono.service.metric.MetricsTags.ProcessingOutcome;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tags;
+import io.micrometer.core.instrument.Timer;
+import io.micrometer.core.instrument.Timer.Sample;
+
+/**
+ * Micrometer based metrics implementation.
+ */
+public class MicrometerBasedMetrics implements Metrics {
+
+    /**
+     * The name of the meter for authenticated connections.
+     */
+    public static final String METER_CONNECTIONS_AUTHENTICATED = "hono.connections.authenticated";
+    /**
+     * The name of the meter for unauthenticated connections.
+     */
+    public static final String METER_CONNECTIONS_UNAUTHENTICATED = "hono.connections.unauthenticated";
+    /**
+     * The name of the meter for recording message payload size.
+     */
+    public static final String METER_MESSAGES_PAYLOAD = "hono.messages.payload";
+    /**
+     * The name of the meter for messages received from devices.
+     */
+    public static final String METER_MESSAGES_RECEIVED = "hono.messages.received";
+    /**
+     * The name of the meter for recording command payload size.
+     */
+    public static final String METER_COMMANDS_PAYLOAD = "hono.commands.payload";
+    /**
+     * The name of the meter for command messages.
+     */
+    public static final String METER_COMMANDS_RECEIVED = "hono.commands.received";
+
+    /**
+     * The meter registry.
+     */
+    protected final MeterRegistry registry;
+
+    private final Map<String, AtomicLong> authenticatedConnections = new ConcurrentHashMap<>();
+    private final AtomicLong unauthenticatedConnections;
+    private final AtomicInteger totalCurrentConnections = new AtomicInteger();
+
+    private LegacyMetrics legacyMetrics;
+
+    /**
+     * Creates a new metrics instance.
+     * 
+     * @param registry The meter registry to use.
+     * @throws NullPointerException if registry is {@code null}.
+     */
+    protected MicrometerBasedMetrics(final MeterRegistry registry) {
+
+        Objects.requireNonNull(registry);
+
+        this.registry = registry;
+        this.unauthenticatedConnections = registry.gauge(METER_CONNECTIONS_UNAUTHENTICATED, new AtomicLong());
+    }
+
+    /**
+     * Sets the legacy metrics.
+     * 
+     * @param legacyMetrics The additional legacy metrics to report.
+     */
+    @Autowired(required = false)
+    public final void setLegacyMetrics(final LegacyMetrics legacyMetrics) {
+        this.legacyMetrics = legacyMetrics;
+    }
+
+    @Override
+    public final void incrementConnections(final String tenantId) {
+
+        Objects.requireNonNull(tenantId);
+        gaugeForTenant(METER_CONNECTIONS_AUTHENTICATED, this.authenticatedConnections, tenantId, AtomicLong::new)
+                .incrementAndGet();
+        this.totalCurrentConnections.incrementAndGet();
+
+    }
+
+    @Override
+    public final void decrementConnections(final String tenantId) {
+
+        Objects.requireNonNull(tenantId);
+        gaugeForTenant(METER_CONNECTIONS_AUTHENTICATED, this.authenticatedConnections, tenantId, AtomicLong::new)
+                .decrementAndGet();
+        this.totalCurrentConnections.decrementAndGet();
+
+    }
+
+    @Override
+    public final void incrementUnauthenticatedConnections() {
+        this.unauthenticatedConnections.incrementAndGet();
+        this.totalCurrentConnections.incrementAndGet();
+    }
+
+    @Override
+    public final void decrementUnauthenticatedConnections() {
+        this.unauthenticatedConnections.decrementAndGet();
+        this.totalCurrentConnections.decrementAndGet();
+    }
+
+    @Override
+    public int getNumberOfConnections() {
+        return this.totalCurrentConnections.get();
+    }
+
+    @Override
+    public Sample startTimer() {
+        return Timer.start(registry);
+    }
+
+    @Override
+    public final void reportTelemetry(
+            final MetricsTags.EndpointType type,
+            final String tenantId,
+            final MetricsTags.ProcessingOutcome outcome,
+            final MetricsTags.QoS qos,
+            final int payloadSize,
+            final Sample timer) {
+
+        reportTelemetry(type, tenantId, outcome, qos, payloadSize, MetricsTags.TtdStatus.NONE, timer);
+    }
+
+    @Override
+    public final void reportTelemetry(
+            final MetricsTags.EndpointType type,
+            final String tenantId,
+            final MetricsTags.ProcessingOutcome outcome,
+            final MetricsTags.QoS qos,
+            final int payloadSize,
+            final MetricsTags.TtdStatus ttdStatus,
+            final Sample timer) {
+
+        Objects.requireNonNull(type);
+        Objects.requireNonNull(tenantId);
+        Objects.requireNonNull(outcome);
+        Objects.requireNonNull(qos);
+        Objects.requireNonNull(ttdStatus);
+        Objects.requireNonNull(timer);
+
+        if (type != MetricsTags.EndpointType.TELEMETRY && type != MetricsTags.EndpointType.EVENT) {
+            throw new IllegalArgumentException("invalid type, must be either telemetry or event");
+        } else if (payloadSize < 0) {
+            throw new IllegalArgumentException("payload size must not be negative");
+        }
+
+        final Tags baseTags = Tags.of(type.asTag())
+                .and(MetricsTags.getTenantTag(tenantId))
+                .and(outcome.asTag());
+
+        timer.stop(this.registry.timer(METER_MESSAGES_RECEIVED, ttdStatus.add(qos.add(baseTags))));
+
+        // record payload size
+        DistributionSummary.builder(METER_MESSAGES_PAYLOAD)
+            .baseUnit("bytes")
+            .minimumExpectedValue(0L)
+            .tags(baseTags)
+            .register(this.registry)
+            .record(payloadSize);
+
+        if (legacyMetrics != null) {
+
+             // Some of the legacy metrics are based on different meter types
+             // than the ones used now. It is necessary to report the legacy
+             // metric in addition to the new ones because the meter types
+             // are incompatible (e.g. duration vs. occurrences).
+            switch(outcome) {
+            case FORWARDED:
+                legacyMetrics.incrementProcessedMessages(type, tenantId);
+                break;
+            case UNDELIVERABLE:
+                legacyMetrics.incrementUndeliverableMessages(type, tenantId);
+                break;
+            case UNPROCESSABLE:
+                // no corresponding legacy metric
+            }
+
+            // The legacy metrics contain a separate metric for
+            // counting the number of messages that contained
+            // an expired TTD value.
+            switch(ttdStatus) {
+            case EXPIRED:
+                legacyMetrics.incrementNoCommandReceivedAndTTDExpired(tenantId);
+                break;
+            default:
+                // nothing to do
+            }
+        }
+    }
+
+    @Override
+    public void reportCommand(
+            final Direction direction,
+            final String tenantId,
+            final ProcessingOutcome outcome,
+            final int payloadSize,
+            final Sample timer) {
+
+        Objects.requireNonNull(direction);
+        Objects.requireNonNull(tenantId);
+        Objects.requireNonNull(outcome);
+        Objects.requireNonNull(timer);
+
+        if (payloadSize < 0) {
+            throw new IllegalArgumentException("payload size must not be negative");
+        }
+
+        final Tags baseTags = Tags.of(direction.asTag())
+                .and(MetricsTags.getTenantTag(tenantId))
+                .and(outcome.asTag());
+
+        timer.stop(this.registry.timer(METER_COMMANDS_RECEIVED, baseTags));
+
+        // record payload size
+        DistributionSummary.builder(METER_COMMANDS_PAYLOAD)
+            .baseUnit("bytes")
+            .minimumExpectedValue(0L)
+            .tags(baseTags)
+            .register(this.registry)
+            .record(payloadSize);
+
+        if (legacyMetrics != null) {
+
+            switch(direction) {
+            case ONE_WAY:
+            case REQUEST:
+                legacyMetrics.incrementCommandDeliveredToDevice(tenantId);
+                break;
+            case RESPONSE:
+                legacyMetrics.incrementCommandResponseDeliveredToApplication(tenantId);
+                break;
+            }
+        }
+    }
+
+    /**
+     * Gets a gauge value for a specific key.
+     * <p>
+     * If no gauge value exists for the given key yet, a new value
+     * is created using the given name and tags.
+     * 
+     * @param <K> The type of the key.
+     * @param <V> The type of the gauge's value.
+     * @param name The metric name.
+     * @param map The map that holds the metric's gauge values.
+     * @param key The key to use for looking up the gauge value.
+     * @param tags The tags to use for creating the gauge if it does not exist yet.
+     * @param instanceSupplier A supplier for creating a new instance of the gauge's value.
+     * @return The gauge value.
+     */
+    protected <K, V extends Number> V gaugeForKey(final String name, final Map<K, V> map, final K key,
+            final Tags tags, final Supplier<V> instanceSupplier) {
+
+        return map.computeIfAbsent(key, a -> {
+
+            return this.registry.gauge(name, tags, instanceSupplier.get());
+
+        });
+
+    }
+
+    /**
+     * Gets a gauge value for a tenant.
+     * <p>
+     * If no gauge value exists for the given tenant yet, a new value
+     * is created using the given name and a tag for the tenant.
+     * 
+     * @param <V> The type of the gauge's value.
+     * @param name The metric name.
+     * @param map The map that holds the metric's gauge values.
+     * @param tenant The tenant to use for looking up the gauge value.
+     * @param instanceSupplier A supplier for creating a new instance of the gauge's value.
+     * @return The gauge value.
+     */
+    protected <V extends Number> V gaugeForTenant(final String name, final Map<String, V> map, final String tenant,
+            final Supplier<V> instanceSupplier) {
+
+        return gaugeForKey(name, map, tenant, Tags.of(MetricsTags.getTenantTag(tenant)), instanceSupplier);
+
+    }
+}
